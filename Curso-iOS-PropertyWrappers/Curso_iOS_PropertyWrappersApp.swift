f48cf788fd712291be3ebc1972b6ab52de9bd232
Eduardo <eduardo@microsoft.com>
2026-02-02 10:07:03 +0100

    Initial Commit

---
+++ Curso-iOS-PropertyWrappers/Curso_iOS_PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_iOS_PropertyWrappersApp.swift
+//  Curso-iOS-PropertyWrappers
+//
+//  Created by Equipo 2 on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_iOS_PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
